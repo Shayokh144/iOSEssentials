2528ab6d1e9b4bc4ffa34cde061b11cf02551beb
Shayokh144 <shayokhbd144@gmail.com>
2021-12-27 01:05:04 +0600

    main vc added

---
+++ RxSwift/RxToDoList/RxToDoList/controllers/TaskListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TaskListViewController.swift
+//  RxToDoList
+//
+//  Created by Asif on 27/12/21.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    @IBOutlet weak var prioritySegmentedControl : UISegmentedControl!
+    @IBOutlet weak var tableView : UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
