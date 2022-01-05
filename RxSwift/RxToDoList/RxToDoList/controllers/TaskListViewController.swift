cb0d5c18b96ebf39cdb99d161e89d5a1b3b9da85
Shayokh144 <shayokhbd144@gmail.com>
2022-01-05 21:04:25 +0600

    view component added

--- RxSwift/RxToDoList/RxToDoList/controllers/TaskListViewController.swift
+++ RxSwift/RxToDoList/RxToDoList/controllers/TaskListViewController.swift
@@ -7,17 +7,33 @@
 
 import UIKit
 
-class TaskListViewController: UIViewController {
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     @IBOutlet weak var prioritySegmentedControl : UISegmentedControl!
     @IBOutlet weak var tableView : UITableView!
+    let tableCellId = "taskTableCell"
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureTableView()
     }
     
 
+    func configureTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: tableCellId, for: indexPath)
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
