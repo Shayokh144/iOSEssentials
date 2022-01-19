4ec67f5fd2f783bee37c759658a6b6e038751773
Shayokh144 <shayokhbd144@gmail.com>
2022-01-19 23:59:49 +0600

    base ui for news app

---
+++ RxSwift/NewsForYou/NewsForYou/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  NewsForYou
+//
+//  Created by Asif on 18/1/22.
+//
+
+import Foundation
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.navigationController?.navigationBar.barTintColor = UIColor.green
+    }
+
+
+}
