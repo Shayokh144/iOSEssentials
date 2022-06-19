ddcdcfb13cef0b52fb7c52fa597572a43bcdba4a
“taher” <taher@nimblehq.co>
2022-06-20 01:06:55 +0700

    showing temperature and humidity done

---
+++ RxSwift/WatherToday/WatherToday/Extension/URL+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  URL+Extension.swift
+//  WatherToday
+//
+//  Created by nimble on 19/6/22.
+//
+
+import Foundation
+
+extension URL {
+    static func URLForWeatherApi(for city: String) -> URL? {
+        return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=34e43ac725f3679763f219f9d2de0801&units=metric")
+
+    }
+}
--- RxSwift/WatherToday/WatherToday/view/ViewController.swift
+++ RxSwift/WatherToday/WatherToday/view/ViewController.swift
@@ -6,45 +6,71 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
+
+    let disposeBag: DisposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        displayWeather(weatherData: nil)
+        textField.rx.controlEvent(.editingDidEndOnExit)
+            .asObservable()
+            .withUnretained(self)
+            .map { _ in self.textField.text }
+            .subscribe(
+                onNext: { cityName in
+                    if let city = cityName {
+                        if city.count > 2 {
+                            self.fetchWeather(for: city)
+                        } else {
+                            self.displayWeather(weatherData: nil)
+                        }
+                    }
+                }
+            ).disposed(by: disposeBag)
     }
 
+    private func displayWeather(weatherData: Weather?) {
+        if let weather = weatherData {
+            temperatureLabel.text = "\(weather.temp) ℃"
+            humidityLabel.text = "\(weather.humidity) %"
+        }
+        else {
+            temperatureLabel.text = "_ ℃"
+            humidityLabel.text = "_ %"
+        }
+    }
+
+    private func fetchWeather(for cityName: String) {
+        // this encoding is needed for the city names which have spaces
+        guard let cityEncoded = cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+              let urlData = URL.URLForWeatherApi(for: cityEncoded) else {
+            return
+        }
+        let resource = Resource<WeatherModel>(url: urlData)
+        URLRequest.load(resource: resource)
+            .observe(on: MainScheduler.instance)
+            .subscribe(
+                onNext: { [weak self] result in
+                    let weather = result.main
+                    self?.displayWeather(weatherData: weather)
+                },
+                onError: {error in
+                    print("Error: \(error.localizedDescription)")
+                    self.displayWeather(weatherData: nil)
+                },
+                onCompleted: {
+                    print("completed Data fetching")
+                }
+            )
+            .disposed(by: disposeBag)
+    }
 
 }
-
-extension Int {
-    func formatCurrency() -> String {
-        let formater = NumberFormatter()
-        formater.numberStyle = .currency
-        formater.locale = Locale.current
-        formater.currencySymbol = ""
-        formater.maximumFractionDigits = 0
-        guard let currencyNumber = formater.string(from: NSNumber(value: self)) else { return "" }
-        return "\(currencyNumber)"
-    }
-
-    func formatCurrencyOld(showUnit: Bool = true) -> String {
-      let formater = NumberFormatter()
-      formater.decimalSeparator = ","
-      formater.numberStyle = .decimal
-      formater.locale = Locale.current
-      formater.currencySymbol = ""
-      formater.allowsFloats = false
-      let unit = showUnit ? " บาท" : ""
-
-      if let currencyNumber = formater.string(from: NSNumber(value: self)) {
-        return "\(currencyNumber)\(unit)"
-      } else {
-        let currencyFormatter = "\(unit)"
-        return String(format: currencyFormatter, self)
-      }
-    }
-}
-
