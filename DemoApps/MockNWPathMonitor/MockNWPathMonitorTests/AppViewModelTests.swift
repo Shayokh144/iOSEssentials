04b28eba140a75c72c6ee24a9ed966d510f7ece0
Shayokh144 <shayokhbd144@gmail.com>
2023-10-31 14:29:31 +0700

    Update test

--- DemoApps/MockNWPathMonitor/MockNWPathMonitorTests/AppViewModelTests.swift
+++ DemoApps/MockNWPathMonitor/MockNWPathMonitorTests/AppViewModelTests.swift
@@ -7,11 +7,13 @@
 
 @testable import MockNWPathMonitor
 import XCTest
+import Combine
 
 final class AppViewModelTests: XCTestCase {
 
     var sut: AppViewModel!
     var mockNetworkPathMonitor: MockNetworkPathMonitor!
+    var cancellable = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -27,23 +29,21 @@ final class AppViewModelTests: XCTestCase {
 
     func testNetworkStatusWhenInternetIsConnected() throws {
         mockNetworkPathMonitor.currentNetworkStatus = .satisfied
-        let expectation = XCTestExpectation(description: "testNetworkStatusWhenInternetIsConnected")
-        mockNetworkPathMonitor.start(queue: DispatchQueue.global())
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 2.0)
-        XCTAssertTrue(sut.networkStatus == .connected)
+        sut.$networkStatus
+            .dropFirst()
+            .sink { [weak self] _ in
+                XCTAssertTrue(self?.sut.networkStatus == .connected)
+            }
+            .store(in: &cancellable)
     }
 
     func testNetworkStatusWhenInternetIsNotConnected() throws {
         mockNetworkPathMonitor.currentNetworkStatus = .unsatisfied
-        let expectation = XCTestExpectation(description: "testNetworkStatusWhenInternetIsNotConnected")
-        mockNetworkPathMonitor.start(queue: DispatchQueue.global())
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            expectation.fulfill()
-        }
-        wait(for: [expectation], timeout: 2.0)
-        XCTAssertTrue(sut.networkStatus == .notConnected)
+        sut.$networkStatus
+            .dropFirst()
+            .sink { [weak self] _ in
+                XCTAssertTrue(self?.sut.networkStatus == .notConnected)
+            }
+            .store(in: &cancellable)
     }
 }
