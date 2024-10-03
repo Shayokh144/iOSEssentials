24bf27bcf87a52a6e35813e3f87a5c8673c371a4
Shayokh144 <shayokhbd144@gmail.com>
2024-10-03 13:45:32 +0700

    Added new project for testing iOS 15 UI issue with divider in list

---
+++ SwiftUI/projects/ScrollWithDivider/ScrollWithDivider/ListVStackView.swift
@@ -0,0 +1,106 @@
+//
+//  ListVStackView.swift
+//  ScrollWithDivider
+//
+//  Created by Taher's nimble macbook on 2/10/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    var body: some View {
+        TabView {
+            VStackForEachScrollView()
+                .tabItem {
+                    Label("ScrollView", systemImage: "list.bullet")
+                }
+            ListVStackView()
+                .tabItem {
+                    Label("List", systemImage: "list.dash")
+                }
+        }
+    }
+}
+
+struct ListVStackView: View {
+    
+    @State private var numberOfItem = 4
+    @State private var numberText = ""
+    
+    var body: some View {
+        VStack(spacing: .zero) {
+            Text("This is List")
+                .padding()
+            TextField("Change Number of Item", text: $numberText)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+            
+            ScrollViewReader { proxy in
+                List(0...numberOfItem, id: \.self) { num in
+                    VStack(spacing: .zero) {
+                        Text("\(num)")
+                            .id(num)
+                        Divider()
+                            .background(Color.black)
+                            .padding(.vertical)
+                    }
+                    .listRowSpacing(0)
+                    .listRowInsets(EdgeInsets())
+                    .listRowSeparator(.hidden)
+                }
+                .listStyle(.plain)
+                .listSectionSeparator(.hidden)
+                Button("Move to middle", action: {
+                    withAnimation {
+                        proxy.scrollTo(numberOfItem / 2, anchor: .center)
+                    }
+                })
+            }
+        }
+        .padding()
+        .onReceive(numberText.publisher) { _ in
+            numberOfItem = Int(numberText) ?? 4
+        }
+    }
+}
+
+
+struct VStackForEachScrollView: View {
+    
+    @State private var numberOfItem = 4
+    @State private var numberText = ""
+    
+    var body: some View {
+        VStack(spacing: .zero) {
+            Text("This is ScrollView")
+                .padding()
+            TextField("Change Number of Item", text: $numberText)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+            
+            ScrollViewReader { proxy in
+                ScrollView {
+                    VStack(spacing: .zero) {
+                        ForEach((0...numberOfItem), id: \.self) { num in
+                            Text("\(num)")
+                                .id(num)
+                            Divider()
+                                .background(Color.black)
+                                .padding(.vertical)
+                        }
+                    }
+                }
+                Button("Move to middle", action: {
+                    withAnimation {
+                        proxy.scrollTo(numberOfItem / 2, anchor: .center)
+                    }
+                })
+            }
+        }
+        .padding()
+        .onReceive(numberText.publisher) { _ in
+            numberOfItem = Int(numberText) ?? 4
+        }
+    }
+}
---
+++ SwiftUI/projects/ScrollWithDivider/ScrollWithDivider/ScrollWithDividerApp.swift
@@ -0,0 +1,19 @@
+//
+//  ScrollWithDividerApp.swift
+//  ScrollWithDivider
+//
+//  Created by Taher's nimble macbook on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollWithDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ListVStackView()
+//            VStackForEachScrollView()
+            MainView()
+        }
+    }
+}
